b7e766395fb36ba8d4422a8830e3c1c75ee8f3cc
chengze <ze.cheng@thoughtworks.com>
2020-01-31 11:42:04 +0800

    basic scenne navigation

---
+++ Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NumberWizard : MonoBehaviour
+{
+    [SerializeField] int min;
+    [SerializeField] int max;
+    [SerializeField] TextMeshProUGUI guessText;
+    int guess; 
+
+    void Start()
+    {
+        setNextGuess();
+        max = max + 1;
+    }
+
+    void setNextGuess() {
+        guess = (max + min) /2; 
+        guessText.text = guess.ToString();
+    }
+
+    public void onHigherPressed() {
+        min = guess;
+        setNextGuess();
+    }
+
+    public void onLowerPressed() {
+        max = guess;
+        setNextGuess();
+    }
+
+}
